82bd33dff44c8d65b605a92624f60ab6fd22dc1f
plabondatta26 <plabondatta26@gmail.com>
2021-11-27 11:31:39 +0600

    error fixed

--- Handler/models.py
+++ Handler/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 # Create your models here.
-class QrCodeModel(models.Model):
-    image = models.FileField(upload_to='qr_images/')
+# class QrCodeModel(models.Model):
+#     image = models.FileField(upload_to='qr_images/')
--- Handler/views.py
+++ Handler/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
 import qrcode
 from django.http import HttpResponse
 from datetime import datetime
-from .models import *
 import cv2
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
@@ -13,11 +11,9 @@ from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 def create_qrcode(data):
     timestamp = round(datetime.now().timestamp())
     filename = 'media/qr_images/' + str(timestamp) + '.png'
-    # qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
-    # img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer())
-    qr = qrcode.QRCode(version=1, box_size=10, border=4)
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
     qr.add_data(data)
-    img = qr.make_image(fill_color="white", back_color="black")
+    img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer())
     img.save(filename)
     return filename
 
@@ -27,7 +23,6 @@ def decode_qrcode(dirs):
     detector = cv2.QRCodeDetector()
     data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
     if vertices_array is not None:
-        print(data)
         return data
     else:
         return "There was some error"
@@ -37,5 +32,4 @@ def home(request):
     text = 'The install worked successfully! Congratulations!'
     dirs = create_qrcode(text)
     dt = decode_qrcode(dirs)
-    # decode_qrcode('media/qr_images/1637987592.png')
     return HttpResponse(dt)
