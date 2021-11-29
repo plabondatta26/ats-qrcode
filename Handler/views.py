0c85d2ebd8c8079856598f6e516fce7be414413b
plabondatta26 <plabondatta26@gmail.com>
2021-11-29 22:12:23 +0600

    dict key error raise

--- Handler/views.py
+++ Handler/views.py
@@ -28,37 +28,46 @@ def create_qrcode(data):
 
 
 def decode_qrcode(dirs):
-    # try:
     return_data = {}
-    dirs = 'media/qr_up_images/' + dirs
-    data = decode(Image.open(dirs))
-    vcard_data = ''
-    for i in data[0]:
-        vcard_data: str = codecs.decode(i, 'UTF-8')
-        break
-    vcard = vobject.readOne(vcard_data)
-    return_data["Name"] = vcard.contents['fn'][0].value
-    telephone = {}
-    for tel in vcard.contents['tel']:
-        i = dict((key, getattr(tel, key)) for key in dir(tel) if key not in dir(tel.__class__))
-        if len(i["params"]) > 0:
-            for j, k in i["params"].items():
-                print(k[0])
-                if k[0] == "FAX" and i["value"]:
-                    return_data["FAX"] = i["value"]
-                elif k[0] == "VIDEO" and i["value"]:
-                    return_data["Video"] = i["value"]
-        else:
-            return_data["Phone"] = i["value"]
+    try:
 
-    return_data["Email"] = vcard.contents['email'][0].value
-    return_data["Birthday"] = vcard.contents['bday'][0].value
-    return_data["Nickname"] = vcard.contents['nickname'][0].value
-    return_data["Address"] = vcard.contents['adr'][0].value
-    return_data["Website"] = vcard.contents['url'][0].value
-    return return_data
-    # except:
-    #     return "There is some error"
+        dirs = 'media/qr_up_images/' + dirs
+        data = decode(Image.open(dirs))
+        key_list = {'Name': 'Name', 'Phone': 'Phone', 'FAX': 'FAX', 'org': 'Company', 'email': 'Email',
+                    'nickname': 'Nickname', 'adr': 'Address'}
+        vcard_data = ''
+        for i in data[0]:
+            vcard_data: str = codecs.decode(i, 'UTF-8')
+            break
+        vcard = vobject.readOne(vcard_data)
+        return_data["Name"] = vcard.contents['fn'][0].value
+
+        for tel in vcard.contents['tel']:
+            i = dict((key, getattr(tel, key)) for key in dir(tel) if key not in dir(tel.__class__))
+            if len(i["params"]) > 0:
+                for j, k in i["params"].items():
+                    print(k[0])
+                    if k[0] == "FAX" and i["value"]:
+                        return_data["FAX"] = i["value"]
+                    elif k[0] == "VIDEO" and i["value"]:
+                        return_data["Video"] = i["value"]
+            else:
+                return_data["Phone"] = i["value"]
+        for keys in vcard.contents:
+            print(vcard.contents[keys][0].value, '*****', )
+            if vcard.contents[keys][0].value and keys == key_list[keys]:
+                return_data[key_list[keys]] = vcard.contents[keys][0].value
+        # if vcard.contents['bday'][0].value:
+        #     return_data["Birthday"] = vcard.contents['bday'][0].value
+        # if vcard.contents['nickname'][0].value:
+        #     return_data["Nickname"] = vcard.contents['nickname'][0].value
+        # if vcard.contents['adr'][0].value:
+        #     return_data["Address"] = vcard.contents['adr'][0].value
+        # if vcard.contents['url'][0].value:
+        #     return_data["Website"] = vcard.contents['url'][0].value
+        return return_data
+    except KeyError as ke:
+        return return_data
 
 
 def home(request):
@@ -76,7 +85,8 @@ def qr_to_text_view(request):
             data = QrCodeModel.objects.last()
             data = str(data.image.url).split('/')[-1]
             dt = decode_qrcode(data)
-            return render(request, 'qr_to_text_view.html', {'data': dt})
+            return redirect('result', dt, 'text')
+            # return render(request, 'qr_to_text_view.html', {'data': dt})
     return render(request, 'qr_to_text_view.html', {'data': ''})
 
 
